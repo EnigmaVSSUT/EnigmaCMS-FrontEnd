b132d901be8735fc7b1f3729a393905deb36c7ce
Namrata <r.namrata99@gmail.com>
2020-10-18 14:11:13 +0530

    Added a preLoader to the site

--- enigma-web/src/App.js
+++ enigma-web/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Lines} from 'react-preloaders';
+import MainComponent from './components/MainComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MainComponent />
+      <Lines background={'black'} color={'green'} time={2000}/>
+    </React.Fragment>
   );
 }
 
---
+++ enigma-web/src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.jpg';
+import '../App.css';
+
+function MainComponent() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Welcome to Enigma!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default MainComponent;
