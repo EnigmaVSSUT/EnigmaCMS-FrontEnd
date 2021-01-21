fd20056643e3aa4a5b4b361445abc874696d1195
ashuvssut <ashu.khanduala@gmail.com>
2021-01-22 00:07:03 +0530

    initial commit: team

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import MainComponent from './components/Main/MainComponent';
+import Team from './components/Team/Team';
 
 function App() {
   return (
-    <React.Fragment>
-      <MainComponent />
-    </React.Fragment>
+    <Team/>
   );
 }
 
---
+++ src/components/Team/Team.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+// import Navbar from "../Navbar/navbar";
+import { Box, Container, Hidden, makeStyles } from "@material-ui/core";
+import * as colors from "../Auth/colors";
+
+const useStyle = makeStyles(theme => ({
+  
+//   movingBox: isSignUp => {
+//     return {
+//       transform: !isSignUp ? "translateX(0%)" : "translateX(-50%)",
+//       transition: "all 500ms cubic-bezier(0.9, 0, 0.33, 1)",
+//     };
+//   },
+//   [theme.breakpoints.down("xs")]: {
+//     movingBox: isSignUp => {
+//       return {
+//         transform: !isSignUp ? "translateX(20px)" : "translateX(-100%)",
+//         transition: "all 500ms cubic-bezier(0.9, 0, 0.33, 1)",
+//       };
+//     },
+//   },
+}));
+
+function Team() {
+  const classes = useStyle();
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Team;
