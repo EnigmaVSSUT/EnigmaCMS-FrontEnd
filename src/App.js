8a8294338bd036b8e1ce06d12ff1bfe298d76de6
Chandrasekhar Samanta <chandrasekhar.2039@gmail.com>
2021-01-24 22:47:38 +0530

    app is ready

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 
 import React from 'react';
-import Preloaders from "./components/Preloader/preloader.jsx"
-import Navbar from "./components/Navbar/navbar.jsx"
-import Footer from "./components/Footer/Footer.js"
+
 import MainComponent from './components/Main/MainComponent';
-import Contact from "./components/Contact/Contact.js"
-import Auth from "./components/Auth/Auth.jsx"
-import {Switch,Route,}from "react-router-dom";
+
+
 
 function App() {
-  return (
-    <React.Fragment>
-    <Navbar/>
-    <Switch>
-          <Route path="/" exact>
-          <MainComponent />
-          </Route>
-          <Route path="/contact">
-             <Contact/>
-          </Route>
-          <Route path="/auth">
-             <Auth/>
-          </Route>
-          <Route>
-          <MainComponent />
-          </Route>
-        </Switch>
-        <Footer/>
-
-    </React.Fragment>
-
+  return (<>
+ <MainComponent/>
+    </>
   );
 }
 
