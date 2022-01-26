0a3e965ebd0ea3088790663387eae5ee016127ac
Ravindra Nag <ravindranag52@gmail.com>
2022-01-27 02:59:57 +0530

    form slides in from the left side

--- src/components/Events/RegistrationForm.jsx
+++ src/components/Events/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, FormControlLabel, FormLabel, MenuItem, Modal, Radio, RadioGroup, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, FormControl, FormControlLabel, FormLabel, MenuItem, Modal, Radio, RadioGroup, Slide, Stack, TextField, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -69,7 +69,7 @@ const RegistrationForm = ({open, event, handleClose}) => {
             return;
         }
         d.event = event.id
-        // console.log(d)
+        console.log(d)
     }
     
     useEffect(() => {
@@ -90,137 +90,149 @@ const RegistrationForm = ({open, event, handleClose}) => {
                 overflowY: 'scroll'
             }}
         >
-            
-            <Box
-                maxWidth={600}
-                component='form'
-                sx={{
-                    width: '100%',
-                    p: 4,
-                    mx: 'auto',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    bgcolor: '#000000'
-                }}
-                onSubmit={handleSubmit(onSubmit)}
+            <Slide
+                direction='right'
+                in={open}
+                mountOnEnter
+                unmountOnExit
             >
                 <Box
+                    maxWidth={600}
+                    component='form'
                     sx={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        p: 4,
                         display: 'flex',
-                        mb: 2,
-                        alignItems: 'center'
+                        flexDirection: 'column',
+                        bgcolor: '#000000'
                     }}
+                    onSubmit={handleSubmit(onSubmit)}
+
                 >
-                    <Typography
+                    <Box
                         sx={{
-                            flexGrow: 1
+                            display: 'flex',
+                            mb: 2,
+                            alignItems: 'center'
                         }}
-                        variant="h4"
-                        color='#35ff1f'
                     >
-                        Registration Form
-                    </Typography>
-                    <CloseIcon style={{color: '#ffffff', cursor: 'pointer'}} fontSize='large' onClick={handleClose} />
+                        <Typography
+                            sx={{
+                                flexGrow: 1
+                            }}
+                            variant="h4"
+                            color='#35ff1f'
+                        >
+                            Registration Form
+                        </Typography>
+                        <CloseIcon style={{color: '#ffffff', cursor: 'pointer'}} fontSize='large' onClick={handleClose} />
+                    </Box>
+                    
+                    <StyledTextField
+                        variant='outlined'
+                        label='First Name'
+                        { ...register('firstname') }
+                        required
+                    />
+                    <StyledTextField
+                        variant='outlined'
+                        label='Last Name'
+                        { ...register('lastname') }
+                        required
+                    />
+                    <StyledFormControl>
+                        <FormLabel>Gender</FormLabel>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue="Male"
+                            name="gender-group"
+                        >
+                            <FormControlLabel value="Male" control={<Radio />} label="Male" />
+                            <FormControlLabel value="Female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="Other" control={<Radio />} label="Other" />
+                        </RadioGroup>
+                    </StyledFormControl>
+                    <StyledTextField
+                        variant='outlined'
+                        label='Email'
+                        type='email'
+                        { ...register('email') }
+                        required
+                    />
+                    <StyledTextField
+                        variant='outlined'
+                        label='WhatsApp Number'
+                        type='tel'
+                        pattern='[0-9]{10}'
+                        { ...register('whatsapp_no') }
+                        required
+                    />
+                    <StyledTextField
+                        select
+                        variant='outlined'
+                        label='Year'
+                        { ...register('year') }
+                        required
+                        value='1'
+                    >
+                        <MenuItem value='1'>1st</MenuItem>
+                        <MenuItem value='2'>2nd</MenuItem>
+                        <MenuItem value='3'>3rd</MenuItem>
+                        <MenuItem value='4'>4th</MenuItem>
+                    </StyledTextField>
+                    <StyledTextField
+                        select
+                        variant='outlined'
+                        label='Branch'
+                        defaultValue=''
+                        { ...register('branch') }
+                        required
+                    >
+
+                        { branches && branches.map(branch => (
+                            <MenuItem value={branch.name}>{branch.name}</MenuItem>
+                        ))}
+                    </StyledTextField>
+                    {/* radio group : gender */}
+                    
+                    <StyledTextField 
+                        variant='outlined'
+                        label='Registration No'
+                        { ...register('reg_number') }
+                        pattern='[0-9]'
+                        required
+                    />
+
+                    
+
+                    <StyledTextField
+                        variant='outlined'
+                        label='What are your expectations from this event?'
+                        { ...register('expectations') }
+                    />
+                    <Stack direction='row'>
+                        <PrimaryButton
+                            variant='contained'
+                            type='submit'
+                        >
+                            Submit
+                        </PrimaryButton>
+                        <Button
+                            onClick={handleClose}
+                            sx={{
+                                ml: 2,
+                                color: '#fff',
+                            }}
+                        >Cancel</Button>
+                    </Stack>
                 </Box>
-                
-                <StyledTextField
-                    variant='outlined'
-                    label='First Name'
-                    { ...register('firstname') }
-                    required
-                />
-                <StyledTextField
-                    variant='outlined'
-                    label='Last Name'
-                    { ...register('lastname') }
-                    required
-                />
-                <StyledFormControl>
-                    <FormLabel>Gender</FormLabel>
-                    <RadioGroup
-                        row
-                        aria-labelledby="demo-radio-buttons-group-label"
-                        defaultValue="Male"
-                        name="gender-group"
-                    >
-                        <FormControlLabel value="Male" control={<Radio />} label="Male" />
-                        <FormControlLabel value="Female" control={<Radio />} label="Female" />
-                        <FormControlLabel value="Other" control={<Radio />} label="Other" />
-                    </RadioGroup>
-                </StyledFormControl>
-                <StyledTextField
-                    variant='outlined'
-                    label='Email'
-                    type='email'
-                    { ...register('email') }
-                    required
-                />
-                <StyledTextField
-                    variant='outlined'
-                    label='WhatsApp Number'
-                    type='tel'
-                    pattern='[0-9]{10}'
-                    { ...register('whatsapp_no') }
-                    required
-                />
-                <StyledTextField
-                    select
-                    variant='outlined'
-                    label='Year'
-                    { ...register('year') }
-                    required
-                >
-                    <MenuItem value='1'>1st</MenuItem>
-                    <MenuItem value='2'>2nd</MenuItem>
-                    <MenuItem value='3'>3rd</MenuItem>
-                    <MenuItem value='4'>4th</MenuItem>
-                </StyledTextField>
-                <StyledTextField
-                    select
-                    variant='outlined'
-                    label='Branch'
-                    defaultValue=''
-                    { ...register('branch') }
-                    required
-                >
-
-                    { branches && branches.map(branch => (
-                        <MenuItem value={branch.name}>{branch.name}</MenuItem>
-                    ))}
-                </StyledTextField>
-                {/* radio group : gender */}
-                
-                <StyledTextField 
-                    variant='outlined'
-                    label='Registration No'
-                    { ...register('reg_number') }
-                    pattern='[0-9]'
-                    required
-                />
-
-                
-
-                <StyledTextField
-                    variant='outlined'
-                    label='What are your expectations from this event?'
-                    { ...register('expectations') }
-                />
-                <Stack direction='row'>
-                    <PrimaryButton
-                        variant='contained'
-                        type='submit'
-                    >
-                        Submit
-                    </PrimaryButton>
-                    <Button
-                        onClick={handleClose}
-                        sx={{
-                            ml: 2,
-                            color: '#fff',
-                        }}
-                    >Cancel</Button>
-                </Stack>
-            </Box>
+            </Slide>
+            
+            
         </Modal>
     );
 }
