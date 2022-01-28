55c67fc5c1c60003dbf032863a5363eb8d46494a
binayaksadangi <youbinayak@gmail.com>
2022-01-28 17:17:09 +0530

    events section divide

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -5,38 +5,77 @@ import FormModal from "./Form";
 import { Axios } from "../../helpers/AxiosInstance";
 function Events() {
   const [open, setOpen] = useState(false);
-  const [events, setevents] = useState([]);
+  const [upComing, setUpComing] = useState([]);
+  const [current, setCurrent] = useState([]);
+  const [past, setPast] = useState([]);
   useEffect(() => {
     Axios({
       method: "GET",
-      url: "events/list/",
+      url: "events/list/?type=upcoming",
     }).then((res) => {
-      setevents(res.data);
+      setUpComing(res.data);
+    });
+    Axios({
+      method: "GET",
+      url: "events/list/?type=current",
+    }).then((res) => {
+      setCurrent(res.data);
+    });
+    Axios({
+      method: "GET",
+      url: "events/list/?type=past",
+    }).then((res) => {
+      setPast(res.data);
     });
   }, []);
-  const header = {
+
+  const header = [
+    {
     mainHeader: "Upcoming Events",
     text: "Elevate your knowledge by registering for our upcoming events",
-  };
+    },
+    {
+      mainHeader: "Current Events",
+      text: "Our Ongoing evnets are here to help you",
+    },
+    {
+      mainHeader: "Past Events",
+      text: "Check out our past events",
+    }
+]
   return (
     <div>
       <FormModal handeClose={() => setOpen(false)} open={open} />
       <div className={classes.common}>
-        <h3 className={classes.heading}>{header.mainHeader}</h3>
-        <p className={classes.mainContent}>{header.text}</p>
+        <h3 className={classes.heading}>{header[0].mainHeader}</h3>
+        <p className={classes.mainContent}>{header[0].text}</p>
         <div className={classes.commonBorder}></div>
       </div>
       <div className={classes.upcoming}>
-        <EventCard onRegisterClick={() => setOpen(true)} />
+      {upComing && upComing.map((event) => <EventCard event={event} />)}
       </div>
       <div className={classes.common}>
-        <h3 className={classes.heading}>Past Events</h3>
-        <p className={classes.mainContent}>Check out our past events</p>
+        <h3 className={classes.heading}>{header[1].mainHeader}</h3>
+        <p className={classes.mainContent}>{header[1].text}</p>
         <div className={classes.commonBorder}></div>
       </div>
-      <div className={classes.pastEvents}>
-        {events && events.map((event) => <EventCard event={event} />)}
+      <div className={classes.upcoming}>
+      {current && current.map((event) => <EventCard event={event} />)}
       </div>
+
+      {past && 
+      <> 
+      <div className={classes.common}>
+        <h3 className={classes.heading}>{header[2].mainHeader}</h3>
+        <p className={classes.mainContent}>{header[2].text}</p>
+        <div className={classes.commonBorder}></div>
+      </div>
+      <div className={classes.upcoming}>
+       {past.map((event) => <EventCard event={event} />)}
+      </div>
+      </>
+      }
+
     </div>
   );
 }
