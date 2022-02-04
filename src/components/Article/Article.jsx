71dbb5d5ac3dab2d8406ee2747b1504b14b5869d
binayaksadangi <youbinayak@gmail.com>
2022-02-04 13:26:35 +0530

    style added to card

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,51 +1,57 @@
-import React from 'react';
-import Heading from '../Heading/index'
-import ArticleCard from './ArticleCard';
-import {Container, Grid} from '@mui/material'
-import { makeStyles } from '@material-ui/styles';
+import React from "react";
+import Heading from "../Heading/index";
+import ArticleCard from "./ArticleCard";
+import { Container, Grid } from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
 
-const useStyles  =  makeStyles({
-    main:{
-        border:'1px solid red',
-        padding:'10px'
-    },
-    item:{
-        border:'1px solid red',
-        padding:'10px',
-        justifyContent:'space-around'
-
-    },
-    card:{
-        // border:'1px solid red',
-        padding:'10px'
-    }
-})
+const useStyles = makeStyles({
+  main: {
+    //border:'1px solid red',
+    padding: "10px",
+  },
+  item: {
+    //border:'1px solid red',
+    padding: "10px",
+    justifyContent: "space-around",
+  },
+  card: {
+    // border:'1px solid red',
+    padding: "10px",
+  },
+});
 
 const Article = () => {
-    const classes = useStyles(); 
+  const classes = useStyles();
   const articles = [
-      {img:'https://picsum.photos/200',title:"Article 1",id:1},
-      {img:'https://picsum.photos/200',title:"Article 2",id:2},
-      {img:'https://picsum.photos/200',title:"Article 3",id:3},
-      {img:'https://picsum.photos/200',title:"Article 4",id:4},
-      {img:'https://picsum.photos/200',title:"Article 5",id:5}
-  ]  
+    { img: "https://picsum.photos/200", title: "Article 1", id: 1 },
+    { img: "https://picsum.photos/200", title: "Article 2", id: 2 },
+    { img: "https://picsum.photos/200", title: "Article 3", id: 3 },
+    { img: "https://picsum.photos/200", title: "Article 4", id: 4 },
+    { img: "https://picsum.photos/200", title: "Article 5", id: 5 },
+  ];
   return (
-  <Container>
-    <Heading main='Articles'/>
-        <Grid container className={classes.main} flexDirection='column'>
-            <Grid item container xs={12} className={classes.item} flexDirection='row' justifyContent='space-arround'>
-                {articles.map((item)=>{
-                    return(
-                        <Grid item xs={12} md={4} key={item.id} className={classes.card}>
-                            <ArticleCard item={item}/>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+    <Container>
+      <Heading main="Articles" />
+      <Grid container className={classes.main} flexDirection="column">
+        <Grid
+          item
+          container
+          xs={12}
+          className={classes.item}
+          flexDirection="row"
+          justifyContent="space-arround"
+        >
+          {articles.map((item) => {
+            return (
+              <Grid item xs={12} md={4} key={item.id} className={classes.card}>
+                <ArticleCard item={item} />
+              </Grid>
+            );
+          })}
         </Grid>
-  </Container>
-  )
+      </Grid>
+    </Container>
+  );
 };
 
 export default Article;
--- src/components/Article/ArticleCard.jsx
+++ src/components/Article/ArticleCard.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import {Card,CardContent,Typography,CardMedia,CardActions,Button} from '@material-ui/core';
-const ArticleCard = ({item}) => {
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
+const useStyles = makeStyles({
+  title: {
+    color: "#35ff1f",
+    fontWeight: "bold",
+    textAlign: "center",
+    letterSpacing: "1px",
+  },
+  btn: {
+    color: "#35ff1f",
+    borderRadius: "10px",
+    marginBottom: "10px",
+    borderColor: "#35ff1f",
+    "&:hover": {
+      backgroundColor: "#35ff1f",
+      color: "#000",
+    },
+  },
+});
+const ArticleCard = ({ item }) => {
+  const classes = useStyles();
   return (
-      <>
-       <Card>
+    <>
+      <Card style={{ backgroundColor: "#17141d" }}>
         <CardMedia
-            component="img"
-            height="100%"
-            image={item.img}
-            alt="Article"
+          component="img"
+          height="100%"
+          image={item.img}
+          alt="Article"
         />
         <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-                {item.title}
-            </Typography>
+          <Typography variant="h5" component="" className={classes.title}>
+            {item.title}
+          </Typography>
         </CardContent>
-        <CardActions>
-             <Button variant='outlined' size="small">View Article</Button>
+        <CardActions style={{ justifyContent: "center" }}>
+          <Link
+            to={`/articles/${item.title}`}
+            style={{ textDecoration: "none" }}
+          >
+            <Button variant="outlined" size="small" className={classes.btn}>
+              View Article
+            </Button>
+          </Link>
         </CardActions>
-
-       </Card>
-      </>
-  )
+      </Card>
+    </>
+  );
 };
 
 export default ArticleCard;
