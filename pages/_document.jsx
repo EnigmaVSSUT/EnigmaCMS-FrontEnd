5eaef517f3fac33ac85832d44d6db3804662e81f
Ravindra Nag <ravindranag52@gmail.com>
2023-02-06 01:59:07 +0530

    feat: seo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+				<meta name='description' content='We are Enigma. The Official Coding Club of VSSUT, Burla' />
+				<meta name='keywords' content='Enigma, VSSUT, Burla, Enigma VSSUT, enigmavssut, vssut developers'/>
+			</Head>
       <body>
         <Main />
         <NextScript />
