4539c5bae15c839722b6eed23e8c4a72f583d3f0
ravindranag <ravindranag52@gmail.com>
2023-03-22 23:41:23 +0530

    feat: custom scrollbar

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,11 +1,21 @@
 import { Button, Stack, Typography } from "@mui/material"
+import { grey } from "@mui/material/colors"
 
 const Hero = () => {
 	return (
 		<Stack
-			flexGrow={1}
+			height='calc(100vh - 62px - 72px)'
+			sx={{
+				background: `radial-gradient(${grey[200]} 0%, transparent 8%)`,
+				backgroundSize: `5vmin 5vmin`,
+				backgroundPosition: '0 0'
+			}}
 		>
-			
+			<Stack
+
+			>
+
+			</Stack>
 		</Stack>
 	)
 }
