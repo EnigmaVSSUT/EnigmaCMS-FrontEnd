ea12ce3811bcf6e82035094f297cd4fa91942140
ravindranag <ravindranag52@gmail.com>
2023-03-25 00:18:58 +0530

    feat: service card

--- components/home/Services.jsx
+++ components/home/Services.jsx
@@ -1,7 +1,25 @@
 import { Stack, Typography } from "@mui/material"
 import EnigmaLink from "../common/EnigmaLink"
+import ServiceCard from "./ServiceCard"
+import { useEffect, useRef, useState } from "react"
 
 const Services = () => {
+	const servicesContainerRef = useRef()
+	const [mousePosition, setMousePosition] = useState({
+		x: 0,
+		y: 0
+	})
+
+	useEffect(() => {
+		servicesContainerRef.current.addEventListener('mousemove', e => {
+			for(const card of servicesContainerRef.current.querySelectorAll('.MuiStack-root')) {
+				const rect = card.getBoundingClientRect(), x = e.clientX - rect.left, y = e.clientY - rect.top
+				card.style.setProperty('--mouse-x', `${x}px`)
+				card.style.setProperty('--mouse-y', `${y}px`)
+			}
+		})
+	}, [])
+
 	return (
 		<Stack
 			minHeight='100vh'
@@ -13,7 +31,31 @@ const Services = () => {
 			>
 				What we do.
 			</Typography>
-			<EnigmaLink />
+			<Stack
+				direction='row'
+				flexWrap='wrap'
+				ref={servicesContainerRef}
+				sx={{
+					'&:hover': {
+						'& .MuiStack-root:before': {
+							opacity: 1
+						}
+					}
+				}}
+				gap='8px'
+				padding='64px 0'
+				flex='300px'
+				maxWidth='916px'
+				alignSelf='center'
+				justifyContent='center'
+			>
+				<ServiceCard />
+				<ServiceCard />
+				<ServiceCard />
+				<ServiceCard />
+				<ServiceCard />
+				<ServiceCard />
+			</Stack>
 		</Stack>
 	)
 }
