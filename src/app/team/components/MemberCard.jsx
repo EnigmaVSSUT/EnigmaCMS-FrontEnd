212c990dacf27edf2754895b6ca665ba7fed2c4a
SySagar <sysagar07@gmail.com>
2023-07-08 02:10:24 +0530

    feat:updated teams page

--- src/app/team/components/MemberCard.jsx
+++ src/app/team/components/MemberCard.jsx
@@ -1,10 +1,20 @@
-import { Avatar, Button, Card, Stack, Typography } from "@mui/material";
+import {
+	Avatar,
+	Button,
+	Card,
+	IconButton,
+	Stack,
+	Typography,
+} from "@mui/material";
 import cardStyles from "../team.module.css";
+import LanguageIcon from "@mui/icons-material/Language";
+import AndroidIcon from "@mui/icons-material/Android";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
 
 export default function MemberCard({ memberImage, memberName }) {
 	const profileRedirect = () => {
 		const newPageUrl =
-		import.meta.env.VITE_WEB_BASE_URL + "/profile/" + memberName;
+			import.meta.env.VITE_WEB_BASE_URL + "/profile/" + memberName;
 		window.open(newPageUrl, "_blank");
 	};
 
@@ -19,6 +29,8 @@ export default function MemberCard({ memberImage, memberName }) {
 				alignItems: "center",
 				justifyContent: "center",
 				position: "relative",
+				minWidth: "300px",
+				minHeight: "350px",
 			}}
 		>
 			<Stack
@@ -42,14 +54,68 @@ export default function MemberCard({ memberImage, memberName }) {
 					src={memberImage}
 				/>
 
-				<Stack direction={"column"} gap={1}>
-					<Typography textAlign={"center"}>{memberName}</Typography>
-					<Typography variant="caption">Full Stack Developer</Typography>
-				</Stack>
+				<Stack
+					className={cardStyles.overlay}
+					position={"absolute"}
+					bottom={0}
+					left={0}
+					right={0}
+					top={"55%"}
+					minHeight={"260px"}
+					direction={"column"}
+				>
+					<Stack
+						className="overlay-body"
+						position={"relative"}
+						gap={2}
+						height={"100%"}
+						marginTop={"1rem"}
+						alignItems={"center"}
+					>
+						<Typography className={cardStyles.memberName} textAlign={"center"} variant="body1">{memberName}</Typography>
+						<Typography variant="caption">Code and coffee is life!</Typography>
+						<Stack direction={"row"} gap={2}>
+							<IconButton
+							className={cardStyles.domainIcon}
+								sx={{
+									border: "1px solid",
+									borderColor: "divider",
+									borderRadius: "50%",
+								}}
+							>
+								<LanguageIcon />
+							</IconButton>
 
-				<Button variant="outlined" onClick={profileRedirect}>
-					View Profile
-				</Button>
+							<IconButton
+								sx={{
+									border: "1px solid",
+									borderColor: "divider",
+									borderRadius: "50%",
+								}}
+							>
+								<AndroidIcon />
+							</IconButton>
+
+							<IconButton
+								sx={{
+									border: "1px solid",
+									borderColor: "divider",
+									borderRadius: "50%",
+								}}
+							>
+								<SmartToyIcon />
+							</IconButton>
+						</Stack>
+
+						<Button
+							sx={{ position: "absolute", bottom: "20px" }}
+							variant="outlined"
+							onClick={profileRedirect}
+						>
+							View Profile
+						</Button>
+					</Stack>
+				</Stack>
 			</Stack>
 		</Card>
 	);
