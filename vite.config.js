83d0e906f75f0686f6bd1f9ebe557228689306cd
komal-Goel-04 <komalgoel9437@gmail.com>
2024-02-23 23:55:00 +0530

    port changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 5000,
+		port: 3000,
 		host: "club.enigma.code",
 		// host: "localhost",
 	},
