f5834bf275482c1f082ffd32e72b4828b7fbced1
Princerey <rohkumar0126@gmail.com>
2024-02-24 00:36:14 +0530

    final changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
-		host: "localhost",
+		host: "club.enigma.code",
 	},
 	optimizeDeps: {
 		include: ["@mui/icons-material"],
